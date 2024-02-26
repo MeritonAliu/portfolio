78014aa3ee9f9394fbb2b7b5b5eb412af0258fc4
MeritonAliu <77863815+MeritonAliu@users.noreply.github.com>
2024-02-26 20:07:25 +0100

    merge about

--- app/page.jsx
+++ app/page.jsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main>
       <div className="landing">
-        <h1>Meriton Aliu</h1>
-        <p className="p-description">young aspiring engineer and developer</p>
+        <h1 className='typewriter'>Meriton Aliu</h1>
+        <p className="p-description">a young <span className='span-p-description'>aspiring</span> <br /> engineer and developer</p>
         <div className="btn-container">
           <Link className="btn" href="https://github.com/MeritonAliu/">
             GitHub
@@ -16,10 +16,41 @@ export default function Home() {
         </div>
         <p className="p-scroll">scroll down for more</p>
       </div>
-      <div className="about"> 
-        <h1>About me</h1>
+      <div className="about">
+        <div className="about-title">
+          <h1>About me</h1>
+        </div>
+
+        <div className="about-p-container">
+          <div className="about-p-child">
+            <p className="about-p-title">My <span className='span-p-description'>person</span> <br /></p>
+            <p>
+              I am 20 years young <br />
+              I am based in Switzerland, Zürich <br />
+            </p>
+          </div>
+
+          <div className="about-p-child">
+            <p className="about-p-title">My current <span className='span-p-description'>situation</span> <br /></p>
+            <p>
+              Trained electronics engineer EFZ at Bruke Switzerland AG
+              Currently absolving the technical vocational baccalaureate. <br />
+              My next goal is to start the bachelors degree in computer science at the ZHAW in Zürich. <br />
+            </p>
+          </div>
+          <div className="about-p-child">
+            <p className="about-p-title">What i <span className='span-p-description'>like</span> <br /></p>
+            <p>
+              - engineeering and development <br />
+              - indie games <br />
+              - history and geography <br />
+              - art and music <br />
+            </p>
+          </div>
+        </div>
+
       </div>
-      <div className="projects"> 
+      <div className="projects">
         <h1>My projects</h1>
       </div>
     </main>
