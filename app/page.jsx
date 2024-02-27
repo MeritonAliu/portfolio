ba028c939a589383f1194f44f27c36ec0e505101
Toni <77863815+MeritonAliu@users.noreply.github.com>
2024-02-27 05:17:41 +0100

    footer

--- app/page.jsx
+++ app/page.jsx
@@ -115,7 +115,15 @@ export default function Home() {
         </div>
       </div>
       <footer>
-        <h1>Currently under <span className='span-p-description'>contruction</span></h1>
+        <div className="p-footer"> 
+          <p>Meriton Aliu</p>
+        </div>
+        <div className="h-footer"> 
+          <h1>Currently under <span className='span-p-description'>contruction</span></h1>
+        </div>
+        <div className="p-footer"> 
+          <p>2024</p>
+        </div>
       </footer>
     </main>
   );
