c20621b30b4deaa8d2c60f1f8548a82247df6848
MeritonAliu <77863815+MeritonAliu@users.noreply.github.com>
2024-02-27 22:56:30 +0100

    motion

--- app/components/transition.jsx
+++ app/components/transition.jsx
@@ -6,9 +6,9 @@ import { motion } from "framer-motion";
 export default function Transition({ children }) {
   return (
     <motion.div
-      initial={{ y: 40, opacity: 0 }}
+      initial={{ y: 100, opacity: 0 }}
       animate={{ y:0, opacity: 1 }}
-      exit={{ ease: 'easeInOut', opacity: 0, duration: 3}}
+      exit={{ ease: 'easeInOut', opacity: 0, duration: 4}}
     >
       {children}
     </motion.div>
