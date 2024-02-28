821e3e011c15104109e4c2a7751627c9ffe38dc6
MeritonAliu <77863815+MeritonAliu@users.noreply.github.com>
2024-02-28 10:16:17 +0100

    fix

--- app/page.jsx
+++ app/page.jsx
@@ -4,11 +4,9 @@ import About from './components/about';
 import Projects from './components/projects';
 import Footer from './components/footer';
 import Contact from './components/contact';
-import useViewportHeight from './hooks/useViewportHeight';
 
 export default function Home() {
-  useViewportHeight();
-  
+
   return (
     <main>
       <Landing />
