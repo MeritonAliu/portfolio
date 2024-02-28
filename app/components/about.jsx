419fb9805793de63906a102ffaf15757c1dcd937
MeritonAliu <77863815+MeritonAliu@users.noreply.github.com>
2024-02-28 04:37:19 +0100

    save

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -8,7 +8,7 @@ export default function About() {
           <p className="name">I am Meriton Aliu </p>
           <p className="age">20 years old </p>
           <p className="location">From Switzerland, Zürich</p>
-          <p className="apprentice">Apprenticeship done as a electronics engineer at Bruker Switzerland</p>
+          <p className="apprentice">Apprenticeship done as an electronics engineer at Bruker Switzerland</p>
           <p className="bm">Currently absolving a technical vocational baccalaureate</p>
           <p className="bachelor">From September i will begin my bachelor in computer science</p>
           <p className="skills">Skills: C, C#, EagleCAD, Linux, Git, Pyhton, Javascript </p>
