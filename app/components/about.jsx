f2a892dec2e778b8090d0473eed15abb1a49010e
MeritonAliu <77863815+MeritonAliu@users.noreply.github.com>
2024-02-28 03:12:18 +0100

    about me re align

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -3,41 +3,16 @@ export default function About() {
   return (
 
     <div id='about' className="about">
-      <div className="subpage-title">
-        <h1>About me</h1>
-      </div>
-      <div className="about-divider">
-        <div className="about-p-container">
-          <div className="about-p-child">
-            <p className="about-p-title">My <span className='span-p-description'>person</span> <br /></p>
-            <p>
-              I am 20 years young <br />
-              I am based in Switzerland, Zürich <br />
-            </p>
-          </div>
-
-          <div className="about-p-child">
-            <p className="about-p-title">My current <span className='span-p-description'>situation</span> <br /></p>
-            <p>
-              Trained electronics engineer EFZ at Bruker Switzerland AG
-              Currently absolving the technical vocational baccalaureate. <br />
-              My next goal is to start the bachelors degree in computer science at the ZHAW in Zürich. <br />
-            </p>
-          </div>
-          <div className="about-p-child">
-            <p className="about-p-title">What i <span className='span-p-description'>like</span> <br /></p>
-            <p>
-              - engineeering and development <br />
-              - indie games <br />
-              - history and geography <br />
-              - art and music <br />
-            </p>
-          </div>
+      <h1 className="subpage-title">About me</h1>
+        <div className="about-container">
+          <p className="name">I am Meriton Aliu </p>
+          <p className="age">20 years old </p>
+          <p className="location">From Switzerland, Zürich</p>
+          <p className="apprentice">Apprenticeship done as a electronics engineer at Bruker Switzerland</p>
+          <p className="bm">Currently absolving a technical vocational baccalaureate</p>
+          <p className="bachelor">From September i will begin my bachelor in computer science</p>
+          <p className="skills">Skills: C, C#, EagleCAD, Linux, Git, Pyhton, Javascript </p>
         </div>
-        <div className="contactme">
-          <p>Contact me</p>
-        </div>
-      </div>
     </div>
   );
 }
