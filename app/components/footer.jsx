b1cacc283582b166a6277c91249715eeb1419a60
MeritonAliu <77863815+MeritonAliu@users.noreply.github.com>
2024-02-28 01:23:13 +0100

    footer-responsive

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
         <p>Meriton Aliu</p>
       </div>
       <div>
-        <h1>Currently under <span className='span-p-description'>contruction</span></h1>
+        <p>Currently under <span className='span-p-description'>contruction</span></p>
       </div>
       <div>
         <p>2024</p>
