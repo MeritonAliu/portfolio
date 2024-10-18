541a405d03b64c278134042c6588c3a79efcd316
MeritonAliu <tonialiu03@gmail.com>
2024-10-19 00:34:56 +0200

    remove about

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,5 @@
 'use client';
 import Landing from './components/landing';
-import About from './components/about';
 import Projects from './components/projects';
 import Footer from './components/footer';
 import Contact from './components/contact';
@@ -10,7 +9,6 @@ export default function Home() {
   return (
     <main>
       <Landing />
-      <About />
       <Projects />
       <Contact />
       <Footer />
