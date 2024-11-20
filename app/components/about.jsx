85333acef47dc424b2632cb9efc1fbba93a488b0
MeritonAliu <tonialiu03@gmail.com>
2024-11-20 21:45:02 +0100

    update data

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -4,15 +4,16 @@ export default function About() {
 
     <div id='about' className="about">
       <h1 className="subpage-title">About me</h1>
-        <div className="about-container">
-          <p className="name">I am Meriton Aliu </p>
-          <p className="age">20 years old </p>
-          <p className="location">From Switzerland, Zürich</p>
-          <p className="apprentice">Apprenticeship done as an electronics engineer at Bruker Switzerland</p>
-          <p className="bm">Currently absolving a technical vocational baccalaureate</p>
-          <p className="bachelor">From September i will begin my bachelor in computer science</p>
-          <p className="skills">Skills: C, C#, EagleCAD, Linux, Git, Pyhton, Javascript </p>
-        </div>
+      <div className="about-container">
+        <p className="name">I am Meriton Aliu</p>
+        <p className="age">21 years old</p>
+        <p className="location">From Switzerland, Volketswil (near Zürich)</p>
+        <p className="apprentice">Completed an apprenticeship as an Electronics Engineer EFZ at Bruker Switzerland AG</p>
+        <p className="bm">Completed a technical vocational baccalaureate at Berufsfachschule Uster</p>
+        <p className="experience">Currently working as an Intern Application Developer at Hitachi Energy Switzerland AG</p>
+        <p className="bachelor">Simultaneously pursuing a part-time Bachelor of Science in Computer Science at ZHAW Winterthur</p>
+        <p className="skills">Skills: C, C#, Eagle CAD, Python, SQL, Git, Azure DevOps, Power BI, Linux, HTML, CSS, JavaScript</p>
+      </div>
     </div>
   );
 }
