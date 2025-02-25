7a0300597bf5b5119be5bf9b650181b34e1aa7f6
MeritonAliu <tonialiu03@gmail.com>
2025-02-26 00:42:44 +0100

    Clean Setup

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,22 @@
-'use client';
-import Landing from './components/landing';
-import About from './components/about';
-import Projects from './components/projects';
-import Footer from './components/footer';
-import Contact from './components/contact';
+// import Landing from './components/landing';
+// import About from './components/about';
+// import Projects from './components/projects';
+// import Footer from './components/footer';
+// import Contact from './components/contact';
 
 export default function Home() {
 
   return (
     <main>
-      <Landing />
+      <h1>Meriton Aliu</h1>
+      <h2>A young aspiring engineer and developer</h2>
+
+
+      {/* <Landing />
       <About />
       <Projects />
       <Contact />
-      <Footer />
+      <Footer /> */}
     </main>
   );
 }
